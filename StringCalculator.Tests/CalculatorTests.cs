0356d97eb5f8f50347423a249f94ff91f7c77216
Ilker Eker <lkrkr@hotmail.com>
2022-09-27 15:33:49 +0300

    Add project files.

---
+++ StringCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,88 @@
+﻿using FluentAssertions;
+
+namespace StringCalculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData("",0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void Add_AddsUpToTwoNumber_WhenStringIsValid(string calculation,int expected)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+             result.Should().Be(expected);
+
+        }
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("10,90,10,20", 130)]
+        public void Add_AddsUpToAnyNumber_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+            result.Should().Be(expected);
+
+        }
+        [Theory]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("10\n90,10,20", 130)]
+        public void Add_AddsNumbersUsingNewLineDelimeter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+            result.Should().Be(expected);
+
+        }
+        [Theory]
+        [InlineData("//;\n1;2", 3)]
+        [InlineData("//;\n1;2;4", 7)]
+        public void Add_AddsNumbersUsingCustomDelimeter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+            result.Should().Be(expected);
+
+        }
+        [Theory]
+        [InlineData("1,2,-1", "-1")]
+        [InlineData("//;\n1;-2;-4", "-2,-4")]
+        public void Add_ShouldThrowException_WhenNegativeNumbersAreUsed(string calculation, string negativeNumbers)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+
+            Action action = () => sut.Add(calculation);
+
+
+            //Assert
+
+            action.Should().Throw<Exception>()
+                .WithMessage("Negatives not allowed:"+ negativeNumbers);
+
+        }
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public object Add(string numbers)
+        {
+            var delimeters = new List<char> { ',', '\n'};
+            if (numbers.StartsWith("//"))
+            {
+                var splitOnFirstNewLine = numbers.Split(new[] { '\n'},2);
+                var customDelimiter = splitOnFirstNewLine[0].Replace("//", String.Empty).Single();
+                delimeters.Add(customDelimiter);
+                numbers = splitOnFirstNewLine[1];
+            }
+
+            var splitNumbers = numbers
+                .Split(delimeters.ToArray(),StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            var negativeNumbers = splitNumbers.Where(x => x < 0).ToList();
+            //var negativeNumbers = new List<int>();
+            //foreach (var potentiallyNegativeNumber in splitNumbers)
+            //{
+            //    if (potentiallyNegativeNumber < 0 )
+            //    {
+            //        negativeNumbers.Add(potentiallyNegativeNumber); 
+            //    }
+            //}
+            if (negativeNumbers.Any())
+            {
+                throw new Exception("Negatives not allowed:" + string.Join(",", negativeNumbers));
+            }
+
+            return splitNumbers.Sum();
+        }
+    }
+}
